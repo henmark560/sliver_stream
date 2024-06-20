da2211c87ff3faefdbe800450f89bd1310247322
henmark560 <142089604+henmark560@users.noreply.github.com>
2024-06-20 13:32:14 +0100

    hh

--- sliver_stream/client/assets/js/scripts/profile.js
+++ sliver_stream/client/assets/js/scripts/profile.js
@@ -27,7 +27,7 @@ const username = localStorage.getItem("username");
         document.getElementById("email").innerText = data.email;
         document.getElementById("mobile").innerText = data.mobile;
         document.getElementById("country").innerText = data.country;
-        document.getElementById("acc").innerText = data.account_num;
+        document.getElementById("acc").innerText = data.acc_num;
       } else {
         // Redirect to the login.html page if the user is not logged in
         window.location.href = "login.html";
